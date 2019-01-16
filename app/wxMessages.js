2816ca30f978cedbc2c01ee9cd2c0ad0e8cd1447
Vic_Wei <w_k0205@sina.cn>
2019-01-16 17:06:44 +0800

    Update wxMessages.js

--- app/wxMessages.js
+++ app/wxMessages.js
@@ -147,7 +147,7 @@
         __vue_exports__ = __webpack_require__(3)
 
         /* template */
-        var __vue_template__ = __webpack_require__(7)
+        var __vue_template__ = __webpack_require__(4)
         __vue_options__ = __vue_exports__ = __vue_exports__ || {}
         if (
             typeof __vue_exports__.default === "object" ||
@@ -163,10 +163,10 @@
         if (typeof __vue_options__ === "function") {
             __vue_options__ = __vue_options__.options
         }
-        __vue_options__.__file = "/usr/src/app/raw/130413bafa156c3049db7533b808d533/App.vue"
+        __vue_options__.__file = "/usr/src/app/raw/022d51dc57234149d456133fffdfeec8/App.vue"
         __vue_options__.render = __vue_template__.render
         __vue_options__.staticRenderFns = __vue_template__.staticRenderFns
-        __vue_options__._scopeId = "data-v-691e3947"
+        __vue_options__._scopeId = "data-v-038dd310"
         __vue_options__.style = __vue_options__.style || {}
         __vue_styles__.forEach(function(module) {
             for (var name in module) {
@@ -187,19 +187,27 @@
     (function(module, exports) {
 
         module.exports = {
-            "group": {
-                "marginLeft": "32",
-                "marginRight": "32",
-                "marginBottom": "32"
+            "cell": {
+                "flexDirection": "row"
             },
-            "title": {
-                "fontSize": "45",
-                "color": "#41B883"
+            "B-box": {
+                "width": "180",
+                "paddingTop": "15",
+                "paddingBottom": "15",
+                "paddingLeft": "15",
+                "paddingRight": "15"
             },
-            "count": {
-                "marginTop": "6",
-                "fontSize": "28",
-                "color": "#888888"
+            "B-title": {
+                "width": "180",
+                "fontSize": "30",
+                "textAlign": "center",
+                "paddingTop": "15",
+                "color": "#999999"
+            },
+            "B-icon": {
+                "width": "140",
+                "height": "140",
+                "marginLeft": "20"
             }
         }
 
@@ -212,229 +220,88 @@
         "use strict";
 
 
-        var _stringify = __webpack_require__(4);
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
 
-        var _stringify2 = _interopRequireDefault(_stringify);
-
-        function _interopRequireDefault(obj) {
-            return obj && obj.__esModule ? obj : {
-                default: obj
-            };
-        }
-
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-
-        var stream = weex.requireModule('stream');
-        module.exports = {
+        exports.default = {
             data: function data() {
                 return {
-                    getJsonpResult: 'loading...',
-                    getResult: 'loading...',
-                    postResult: 'loading...',
-                    putResult: 'loading...',
-                    deleteResult: 'loading...',
-                    headResult: 'loading...',
-                    patchResult: 'loading...'
+                    row1: [{
+                        title: '腔调掌柜',
+                        icon: 'http://img.alicdn.com/tfs/TB1sWLoRVXXXXbdXXXXXXXXXXXX-140-140.png'
+                    }, {
+                        title: '腔调掌柜',
+                        icon: 'http://gw.alicdn.com/tfs/TB10.R_SpXXXXbtXXXXXXXXXXXX-140-140.png'
+                    }, {
+                        title: '腔调掌柜',
+                        icon: 'http://img.alicdn.com/tfs/TB1fRVASpXXXXXdXXXXXXXXXXXX-140-140.png'
+                    }, {
+                        title: '腔调掌柜',
+                        icon: 'http://img.alicdn.com/tfs/TB1_TkdPVXXXXcJXXXXXXXXXXXX-140-140.png'
+                    }],
+                    row2: [{
+                        title: '腔调掌柜',
+                        icon: 'http://img.alicdn.com/tfs/TB1057iSXXXXXaXaVXXXXXXXXXX-140-140.png'
+                    }, {
+                        title: '腔调掌柜',
+                        icon: 'http://img.alicdn.com/tfs/TB1tN6cRVXXXXbiXpXXXXXXXXXX-140-140.png'
+                    }, {
+                        title: '腔调掌柜',
+                        icon: 'http://img.alicdn.com/tfs/TB1oYDaSXXXXXa7XpXXXXXXXXXX-140-140.png'
+                    }, {
+                        title: '腔调掌柜',
+                        icon: 'http://img.alicdn.com/tfs/TB1vRGhSpXXXXaLXpXXXXXXXXXX-140-140.png'
+                    }]
                 };
-            },
-            created: function created() {
-                var me = this;
-                var GET_URL_JSONP = 'http://jsfiddle.net/echo/jsonp/?callback=anything&result=content_in_response';
-                var GET_URL = 'http://httpbin.org/get';
-                var POST_URL = 'http://httpbin.org/post';
-                var PUT_URL = 'http://httpbin.org/put';
-                var DELETE_URL = 'http://httpbin.org/delete';
-                var HEAD_URL = 'http://httpbin.org/status/418';
-                var PATCH_URL = 'http://httpbin.org/patch';
-
-                stream.fetch({
-                    method: 'GET',
-                    url: GET_URL_JSONP,
-                    type: 'jsonp'
-                }, function(ret) {
-                    if (!ret.ok) {
-                        me.getJsonpResult = "request failed";
-                    } else {
-                        console.log('get:' + ret);
-                        me.getJsonpResult = (0, _stringify2.default)(ret.data);
-                    }
-                }, function(response) {
-                    console.log('get jsonp in progress:' + response.length);
-                    me.getJsonpResult = "bytes received:" + response.length;
-                });
-
-                stream.fetch({
-                    method: 'GET',
-                    url: GET_URL,
-                    type: 'json'
-                }, function(ret) {
-                    if (!ret.ok) {
-                        me.getResult = "request failed";
-                    } else {
-                        console.log('get:' + ret);
-                        me.getResult = (0, _stringify2.default)(ret.data);
-                    }
-                }, function(response) {
-                    console.log('get in progress:' + response.length);
-                    me.getResult = "bytes received:" + response.length;
-                });
-
-                stream.fetch({
-                    method: 'POST',
-                    url: POST_URL,
-                    type: 'json'
-                }, function(ret) {
-                    if (!ret.ok) {
-                        me.postResult = "request failed";
-                    } else {
-                        console.log('get:' + (0, _stringify2.default)(ret));
-                        me.postResult = (0, _stringify2.default)(ret.data);
-                    }
-                }, function(response) {
-                    console.log('get in progress:' + response.length);
-                    me.postResult = "bytes received:" + response.length;
-                });
-
-                stream.fetch({
-                    method: 'PUT',
-                    url: PUT_URL,
-                    type: 'json'
-                }, function(ret) {
-                    if (!ret.ok) {
-                        me.putResult = "request failed";
-                    } else {
-                        console.log('get:' + (0, _stringify2.default)(ret));
-                        me.putResult = (0, _stringify2.default)(ret.data);
-                    }
-                }, function(response) {
-                    console.log('get in progress:' + response.length);
-                    me.putResult = "bytes received:" + response.length;
-                });
-
-                stream.fetch({
-                    method: 'DELETE',
-                    url: DELETE_URL,
-                    type: 'json'
-                }, function(ret) {
-
-                    if (!ret.ok) {
-                        me.deleteResult = "request failed";
-                    } else {
-                        console.log('get:' + (0, _stringify2.default)(ret));
-                        me.deleteResult = (0, _stringify2.default)(ret.data);
-                    }
-                }, function(response) {
-                    console.log('get in progress:' + response.length);
-                    me.deleteResult = "bytes received:" + response.length;
-                });
-
-                stream.fetch({
-                    method: 'HEAD',
-                    url: HEAD_URL,
-                    type: 'json'
-                }, function(ret) {
-                    if (ret.statusText !== 'I\'m a teapot') {
-                        me.headResult = "request failed";
-                    } else {
-                        console.log('get:' + (0, _stringify2.default)(ret));
-                        me.headResult = ret.statusText;
-                    }
-                }, function(response) {
-                    console.log('get in progress:' + response.length);
-                    me.headResult = "bytes received:" + response.length;
-                });
-
-                stream.fetch({
-                    method: 'PATCH',
-                    url: PATCH_URL,
-                    type: 'json'
-                }, function(ret) {
-                    if (!ret.ok) {
-                        me.patchResult = "request failed";
-                    } else {
-                        console.log('get:' + (0, _stringify2.default)(ret));
-                        me.patchResult = (0, _stringify2.default)(ret.data);
-                    }
-                }, function(response) {
-                    console.log('get in progress:' + response.length);
-                    me.patchResult = "bytes received:" + response.length;
-                });
             }
         };
 
         /***/
     }),
     /* 4 */
-    /***/
-    (function(module, exports, __webpack_require__) {
-
-        module.exports = {
-            "default": __webpack_require__(5),
-            __esModule: true
-        };
-
-        /***/
-    }),
-    /* 5 */
-    /***/
-    (function(module, exports, __webpack_require__) {
-
-        var core = __webpack_require__(6);
-        var $JSON = core.JSON || (core.JSON = {
-            stringify: JSON.stringify
-        });
-        module.exports = function stringify(it) { // eslint-disable-line no-unused-vars
-            return $JSON.stringify.apply($JSON, arguments);
-        };
-
-
-        /***/
-    }),
-    /* 6 */
-    /***/
-    (function(module, exports) {
-
-        var core = module.exports = {
-            version: '2.5.7'
-        };
-        if (typeof __e == 'number') __e = core; // eslint-disable-line no-undef
-
-
-        /***/
-    }),
-    /* 7 */
     /***/
     (function(module, exports) {
 
@@ -443,49 +310,43 @@
                 var _vm = this;
                 var _h = _vm.$createElement;
                 var _c = _vm._self._c || _h;
-                return _c('scroller', [_c('div', {
-                    staticClass: ["group"]
-                }, [_c('text', {
-                    staticClass: ["title"]
-                }, [_vm._v("method = GET")]), _c('text', {
-                    staticClass: ["count"]
-                }, [_vm._v(_vm._s(_vm.getResult))])]), _c('div', {
-                    staticClass: ["group"]
-                }, [_c('text', {
-                    staticClass: ["title"]
-                }, [_vm._v("method = GET / type = jsonp")]), _c('text', {
-                    staticClass: ["count"]
-                }, [_vm._v(_vm._s(_vm.getJsonpResult))])]), _c('div', {
-                    staticClass: ["group"]
-                }, [_c('text', {
-                    staticClass: ["title"]
-                }, [_vm._v("method = POST")]), _c('text', {
-                    staticClass: ["count"]
-                }, [_vm._v(_vm._s(_vm.postResult))])]), _c('div', {
-                    staticClass: ["group"]
-                }, [_c('text', {
-                    staticClass: ["title"]
-                }, [_vm._v("method = PUT")]), _c('text', {
-                    staticClass: ["count"]
-                }, [_vm._v(_vm._s(_vm.putResult))])]), _c('div', {
-                    staticClass: ["group"]
-                }, [_c('text', {
-                    staticClass: ["title"]
-                }, [_vm._v("method = DELETE")]), _c('text', {
-                    staticClass: ["count"]
-                }, [_vm._v(_vm._s(_vm.deleteResult))])]), _c('div', {
-                    staticClass: ["group"]
-                }, [_c('text', {
-                    staticClass: ["title"]
-                }, [_vm._v("method = HEAD")]), _c('text', {
-                    staticClass: ["count"]
-                }, [_vm._v(_vm._s(_vm.headResult))])]), _c('div', {
-                    staticClass: ["group"]
-                }, [_c('text', {
-                    staticClass: ["title"]
-                }, [_vm._v("method = PATCH")]), _c('text', {
-                    staticClass: ["count"]
-                }, [_vm._v(_vm._s(_vm.patchResult))])])])
+                return _c('list', {
+                    staticClass: ["list"]
+                }, [_c('cell', {
+                    staticClass: ["cell"],
+                    appendAsTree: true,
+                    attrs: {
+                        "append": "tree"
+                    }
+                }, _vm._l((_vm.row1), function(item) {
+                    return _c('div', {
+                        staticClass: ["B-box"]
+                    }, [_c('image', {
+                        staticClass: ["B-icon"],
+                        attrs: {
+                            "src": item.icon
+                        }
+                    }), _c('text', {
+                        staticClass: ["B-title"]
+                    }, [_vm._v(_vm._s(item.title))])])
+                })), _c('cell', {
+                    staticClass: ["cell"],
+                    appendAsTree: true,
+                    attrs: {
+                        "append": "tree"
+                    }
+                }, _vm._l((_vm.row2), function(item) {
+                    return _c('div', {
+                        staticClass: ["B-box"]
+                    }, [_c('image', {
+                        staticClass: ["B-icon"],
+                        attrs: {
+                            "src": item.icon
+                        }
+                    }), _c('text', {
+                        staticClass: ["B-title"]
+                    }, [_vm._v(_vm._s(item.title))])])
+                }))])
             },
             staticRenderFns: []
         }
