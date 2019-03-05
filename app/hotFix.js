2f3cdfc4da6bf7cfd4ce46240481ef59d27679e5
Vic_Wei <w_k0205@sina.cn>
2019-03-05 12:15:47 +0800

    Update hotFix.js

--- app/hotFix.js
+++ app/hotFix.js
@@ -2,3 +2,12 @@ fixInstanceMethodReplace('ZSHRestaurantDetailsViewController', 'testAction:', fu
 { 
     console.log('*** -> HotFix is work <- ***'); 
 });
+
+fixInstanceMethodAfter('ZSHRestaurantDetailsViewController', 'initUI', function(instance, originInvocation)
+{ 
+    var naviItem = runInstanceWithNoParamter(instance,'navigationItem');
+    var rightItem = runInstanceWithNoParamter(naviItem,'rightBarButtonItem');
+    runVoidInstanceWithNoParamter(rightItem,'setCustomView:');
+     
+    console.log('*** -> HotFix is work <- ***'); 
+});
