f4df5af46e63f4f299f791b5d85408fc2914ff5c
Vic_Wei <w_k0205@sina.cn>
2019-03-05 11:19:39 +0800

    Update hotFixTest.js

--- app/hotFixTest.js
+++ app/hotFixTest.js
@@ -1,6 +1,4 @@
-fixInstanceMethodAfter ('AppDelegate', 'add2019NewYearLocalPush', function(instance, originInvocation)
+fixInstanceMethodReplace('ZSHRestaurantDetailsViewController', 'testAction:', function(instance, originInvocation, originArguments)
 { 
-    var pushInstance = runClassWithNoParamter ('WKLocalPushNotificationManager', 'sharedInstance');
-    runVoidInstanceWithNoParamter (pushInstance,'iOS8_removeAllLocalNotifications');
     console.log('*** -> HotFix is work <- ***'); 
 });
